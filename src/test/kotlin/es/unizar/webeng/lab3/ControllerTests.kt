9445b4b3e25df9286b8f539fbf99282745f9feef
Celia <796685@unizar.es>
2022-10-26 15:33:55 +0200

    Test

--- src/test/kotlin/es/unizar/webeng/lab3/ControllerTests.kt
+++ src/test/kotlin/es/unizar/webeng/lab3/ControllerTests.kt
@@ -1,6 +1,10 @@
 package es.unizar.webeng.lab3
 
 import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.justRun
+import io.mockk.slot
+import io.mockk.verify
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -12,6 +16,7 @@ import org.springframework.test.web.servlet.delete
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 import org.springframework.test.web.servlet.put
+import java.util.*
 
 private val MANAGER_REQUEST_BODY = { name: String ->
     """
@@ -47,6 +52,15 @@ class ControllerTests {
 
         // SETUP
 
+        val employee = slot<Employee>()
+        every {
+            employeeRepository.save(capture(employee))
+        } answers {
+            employee.captured.copy(id = 1)
+        } andThenAnswer {
+            employee.captured.copy(id = 2)
+        }
+
         mvc.post("/employees") {
             contentType = MediaType.APPLICATION_JSON
             content = MANAGER_REQUEST_BODY("Mary")
@@ -75,12 +89,29 @@ class ControllerTests {
 
         // VERIFY
 
+        verify(exactly = 2) {
+            employeeRepository.save(Employee("Mary", "Manager"))
+        }
+
+
     }
 
     @Test
     fun `GET is safe and idempotent`() {
 
         // SETUP
+        every {
+            employeeRepository.findById(1)
+        } answers {
+            Optional.of(Employee("Mary", "Manager", 1))
+        }
+
+        every {
+            employeeRepository.findById(2)
+        } answers {
+            Optional.empty()
+        }
+
 
         mvc.get("/employees/1").andExpect {
             status { isOk() }
@@ -110,6 +141,20 @@ class ControllerTests {
     fun `PUT is idempotent but not safe`() {
 
         // SETUP
+        val employee = slot<Employee>()
+        every {
+            employeeRepository.save(capture(employee))
+        } answers {
+            employee.captured
+        }
+
+        every {
+            employeeRepository.findById(1)
+        } answers {
+            Optional.empty()
+        } andThenAnswer {
+            Optional.of(Employee("Tom", "Manager", 1))
+        }
 
         mvc.put("/employees/1") {
             contentType = MediaType.APPLICATION_JSON
@@ -138,13 +183,26 @@ class ControllerTests {
         }
 
         // VERIFY
-
+        verify(exactly = 2) {
+            employeeRepository.save(Employee("Tom", "Manager", 1))
+        }
     }
 
     @Test
     fun `DELETE is idempotent but not safe`() {
 
         // SETUP
+        every {
+            employeeRepository.findById(1)
+        } answers {
+            Optional.of(Employee("Tom", "Manager", 1))
+        } andThenAnswer {
+            Optional.empty()
+        }
+
+        justRun {
+            employeeRepository.deleteById(1)
+        }
 
         mvc.delete("/employees/1").andExpect {
             status { isNoContent() }
@@ -155,6 +213,8 @@ class ControllerTests {
         }
 
         // VERIFY
-
+        verify(exactly = 1) {
+            employeeRepository.deleteById(1)
+        }
     }
 }
